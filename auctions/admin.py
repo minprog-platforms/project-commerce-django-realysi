df925618f4ab16aa7395bcb68d1157f714791be4
realysi <116087413+realysi@users.noreply.github.com>
2022-12-14 22:10:04 +0100

    Models basis + admin (superuser)
    Basis models gedaan.
    Superuser aangemaakt (Yanick_idsinga: django123)

--- auctions/admin.py
+++ auctions/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
+from.models import User, Category, Listing
 
-# Register your models here.
+# Register your models here -> so you can see it in admin
+admin.site.register(User)
+admin.site.register(Category)
+admin.site.register(Listing)
--- auctions/models.py
+++ auctions/models.py
@@ -4,3 +4,19 @@ from django.db import models
 
 class User(AbstractUser):
     pass
+
+class Category(models.Model):
+    name_category = models.CharField(max_length = 69)
+
+    def __str__(self):
+        return self.name_category
+
+
+class Listing(models.Model):
+    title = models.CharField(max_length = 69)
+    description = models.CharField(max_length = 420)
+    url_image = models.CharField(max_length=4200)
+    price = models.FloatField()
+    active = models.BooleanField(default=True)
+    beheerder = models.ForeignKey(User, on_delete=models.CASCADE, blank=True, null=True, related_name="user")
+    category = models.ForeignKey(Category, on_delete=models.CASCADE, blank=True, null=True, related_name="category")
